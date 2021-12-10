c7c8b6b318d99d4cd439c41d6db031c43e0bd432
Tim <timothy.george.taylor@gmail.com>
2021-12-10 16:39:27 -0700

    added reviews to posts

---
+++ db/migrate/20211210232056_add_columns_to_post.rb
@@ -0,0 +1,6 @@
+class AddHeaderToPost < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :header, :string
+    add_column :posts, :review, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_195020) do
+ActiveRecord::Schema.define(version: 2021_12_10_232056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2021_12_10_195020) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "review"
+    t.string "header"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
