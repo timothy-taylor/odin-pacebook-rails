b55b0ac074d1b52fed3644f8b58992eb176ff8ec
Tim <timothy.george.taylor@gmail.com>
2021-12-10 17:08:40 -0700

    added validation to review

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
 
   validates :body, presence: true, length: { maximum: 2000 }
   validates :header, allow_blank: true, length: { maximum: 20 }
+  validates :review, allow_blank: true, numericality: { less_than_or_equal_to: 10 }
 
   def fmt_date
     created_at.strftime("%m/%d/%Y %I:%M %p")
